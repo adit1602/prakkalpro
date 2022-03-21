2facaaeca72a9caad32da0f2040e07d3b6c6ff85
Nabil Azzuhdi <101844464+NotApplicable-NA@users.noreply.github.com>
2022-03-21 13:34:42 +0700

    Update programm kalkulator sederhana.cpp

--- Proggram/programm kalkulator sederhana.cpp
+++ Proggram/programm kalkulator sederhana.cpp
@@ -6,60 +6,47 @@ using namespace std;
 
 int main(){
 	
-	int bil1,bil2, pil, loop=1;
+	int bil1,bil2, pil;
 	float hasil;
 	string operasi;
 	
-	while(true){
-		cout<<"PILIH OPERATOR ARITMATIKA"<<endl;
+	cout<<"PILIH OPERATOR ARITMATIKA"<<endl;
 	cout<<"1. Penjumlahan"<<endl;
 	cout<<"2. Pengurangan"<<endl;
 	cout<<"3. Perkalian"<<endl;
 	cout<<"4. Pembagian"<<endl;
 	cout<<"5. Modulus"<<endl;
-	cout<<"6. Keluar"<<endl;
 	cout<<endl;
+	
 	cout<<"Masukan Pilihan : ";
 	cin>>pil;
-	system("cls");
+	cout<<"Masukan Bilangan pertama : ";
+	cin>>bil1;
+	cout<<"Masukan Bilangan kedua : ";
+	cin>>bil2;	
 	
-	if (pil < 6){
-		cout<<"Masukan Bilangan pertama : ";
-		cin>>bil1;
-		cout<<"Masukan Bilangan kedua : ";
-		cin>>bil2;	
-		
-		switch(pil){
-		        case 1 : hasil=bil1+bil2;
-				operasi='+';
-				break;
-				case 2 : hasil=bil1-bil2;
-					operasi='-';
-					break;
-				case 3 : hasil=bil1*bil2;
-					operasi='*';
-					break;
-				case 4 : hasil=bil1/bil2;
-					operasi='/';
-					break;
-				case 5 : hasil=bil1%bil2;
-					 operasi='%';
-					break;
-				default :
-					cout<<"Salah Masukan Operator"<<endl;
-			}
-			cout<<"-----------------------------"<<endl;
-			cout<<"    "<<bil1<<operasi<<bil2<<"="<<hasil<<endl;
-			cout<<"-----------------------------"<<endl;
-			
-			getch();
-			system("cls");
-	}else{
-		break;;
-		}
-	
-	
+	switch(pil){
+	        case 1 : hasil=bil1+bil2;
+			operasi='+';
+			break;
+		case 2 : hasil=bil1-bil2;
+			operasi='-';
+			break;
+		case 3 : hasil=bil1*bil2;
+			operasi='*';
+			break;
+		case 4 : hasil=bil1/bil2;
+			operasi='/';
+			break;
+		case 5 : hasil=bil1%bil2;
+			 operasi='%';
+			break;
+		default :
+			cout<<"Salah Masukan Operator"<<endl;
 	}
+	cout<<"-----------------------------"<<endl;
+	cout<<"    "<<bil1<<operasi<<bil2<<"="<<hasil<<endl;
+	cout<<"-----------------------------"<<endl;
 	
-	
+	getch();
 }
